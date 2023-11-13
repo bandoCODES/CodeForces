d54acfb2f334a44c53016a260fc662341a557aa1
bandoCODES <144690392+bandoCODES@users.noreply.github.com>
2023-11-13 13:16:25 +0000

    Add files via upload

---
+++ long_words.py
@@ -0,0 +1,16 @@
+#long words problem
+
+
+n = int(input())
+for i in range(0,n):
+    word = input()
+    
+
+    if len(word) > 10:
+        print(word[0]+str(len(word)-2)+word[-1])
+    else:
+        print(word)
+        
+
+
+
---
+++ watermelon.py
@@ -0,0 +1,12 @@
+#Watermelon 800
+#w = weight of melon
+
+w = int(input())
+
+if w <=3:
+    print("NO")
+else:
+    if w % 2 == 0:
+        print("YES")
+    else:
+        print("NO")
